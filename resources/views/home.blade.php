852aaba335caf16f7f9c4449c5463b06f6ff243b
konstantin141091 <konstantin.sudakov@bk.ru>
2020-12-17 07:09:07 +0900

    Натянул макет рабочего варианта версти на шаблоны. Пока всё страшно и неочень. Настроил webpack. Стили и js собираються теперь через него

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,42 @@
 @endsection
 
 @section('content')
-    @guest
-        Для участия требуется аутентификация!<br>
-        Имеем трех юзеров LOGIN: user1@mail.ru PASS: 123 <br>
-        LOGIN: user2@mail.ru PASS: 123 <br>
-        LOGIN: user3@mail.ru PASS: 123
 
-    @else
-        <div class="container">
-            <h2>Коробка с подарками!</h2>
-            <div class="box" style="display: flex; flex-direction: column;">
-                <img src="https://www.ljplus.ru/img4/1/0/1001smile/_korobka.jpg" alt="" style="width: 100px;">
+    <div class="content">
+        <div class="content__top">
+            <div class="logo">GiftBox</div>
+            <div class="logo_speach">make your friends happy with gifts they never expect!</div>
+        </div>
+        <div class="lights__top"></div>
+        @guest
+            Для участия требуется аутентификация!<br>
+            Имеем трех юзеров LOGIN: user1@mail.ru PASS: 123 <br>
+            LOGIN: user2@mail.ru PASS: 123 <br>
+            LOGIN: user3@mail.ru PASS: 123
+
+        @else
+
+{{--            <h2>Коробка с подарками!</h2>--}}
+{{--            <div class="box" style="display: flex; flex-direction: column;">--}}
+{{--                <img src="https://www.ljplus.ru/img4/1/0/1001smile/_korobka.jpg" alt="" style="width: 100px;">--}}
+{{--                @if($boxReady)--}}
+{{--                    <a href="{{ route('boxOpen') }}">Открыть коробку!</a>--}}
+{{--                @else--}}
+{{--                    <span>В коробке мало подарков! Нужно хотя бы 3 для открытия.</span>--}}
+{{--                @endif--}}
+{{--            </div>--}}
+
+
+            <div class="content__middle">
+                <a href="#"><img src="img/giftbox_bg.png" alt="open!" class="box_main"></a>
+                <!-- Нужен ли визуальный таймер? Бутстрап можно присунуть -->
+                <div class="timer_open"></div>
+                <!-- закидывание подарков в коробку -->
+                <div class="button_addgift">
+                    <a href="#">
+                        Добавить подарок
+                    </a>
+                </div>
                 @if($boxReady)
                     <a href="{{ route('boxOpen') }}">Открыть коробку!</a>
                 @else
@@ -23,6 +48,7 @@
                 @endif
             </div>
 
-        </div>
-    @endguest
+
+        @endguest
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,67 +17,82 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                @yield('menu')
+        <div class="bg_container"></div>
+        <div class="container">
+            <header>
+                <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                    <div class="container">
+                        @yield('menu')
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- Left Side Of Navbar -->
+                            <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                            </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                            <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                                <!-- Authentication Links -->
+                                @guest
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                                {{ __('Logout') }}
+                                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endguest
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
 
-        <main class="py-4">
-            @if (session('success'))
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{ session('success') }}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
-            @yield('content')
-        </main>
+
+            <main class="py-4">
+
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
+                @yield('content')
+
+            </main>
+            <footer class="footer">
+                <a href="#">about</a>
+                <!--<a href="">faq</a>
+                <a href="">support</a>-->
+            </footer>
+        </div>
     </div>
 </body>
 </html>
