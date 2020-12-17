8af84f796d99c558149f0585fd7eabfc6255faa0
Prohor Sevastianov <sevastyanov.p@oneway.su>
2020-12-17 10:50:03 +0300

    убрал data-toggle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('profile') }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('profile') }}" role="button" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
