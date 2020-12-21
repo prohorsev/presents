bbf5c1082f15a5a56f8578116212fae76f8f52ce
pwnyaka <vas7424@mail.ru>
2020-12-21 17:38:09 +0400

    rooms, users DB tables, room blade template

--- app/Http/Controllers/Room/RoomController.php
+++ app/Http/Controllers/Room/RoomController.php
@@ -3,11 +3,99 @@
 namespace App\Http\Controllers\Room;
 
 use App\Http\Controllers\Controller;
+use App\Room;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
-    public function create() {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
         return view('room.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->except('_token');
+        $data['org_user_id'] = \Auth::id();
+        $room = new Room();
+
+        $result = $room->fill($data)->save();
+
+        if ($result) {
+            return redirect()->route('room.show', ['room' => $room]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $room = Room::query()->find($id);
+        $friends = \DB::table('room_user')->join('users', 'users.id', '=', 'room_user.user_id')->select('name')
+            ->where('room_id', '=', $id)->get();
+//        dd($friends);
+        return view('room.show', [
+            'room' => $room,
+            'friends' => $friends,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
