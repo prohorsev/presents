8821b167d79611d60b9f148c54b09018f55d279a
pwnyaka <vas7424@mail.ru>
2021-01-13 21:00:42 +0400

    room create/edit validation

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Room extends Model
 {
@@ -11,5 +12,35 @@ class Room extends Model
     public function users()
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public static function rules(Room $room)
+    {
+        $tableName = (new Room())->getTable();
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'max:30',
+                Rule::unique('rooms')->ignore($room->id)
+            ],
+            'birthday_person' => "required|alpha|max:50",
+            'birthday_date' => 'required|date|after:today',
+            'birthday_sum' => 'required|numeric|min:1'
+        ];
+    }
+
+    public static function messages()
+    {
+        return [
+            'name.unique' => 'Команда с таким именем уже существует!',
+            'required' => 'Заполните это поле!',
+            'after' => 'Указанная дата уже наступила!',
+            'birthday_person.alpha' => 'В этом поле допускаются только буквенные символы!', // это пока что, потом может почту указывать будем и тд
+            'name.min' => 'Название команды должно содержать не менее трех символов!',
+            'name.max' => 'Название команды слишком длинное!',
+            'birthday_person.max' => '',
+            'birthday_sum.min' => 'Бюджет должен быть больше нуля!'
+        ];
     }
 }
